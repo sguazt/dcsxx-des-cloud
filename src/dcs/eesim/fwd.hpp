60a37180d24d937eb51320e898bf1878336b9468
Sguazt <sguazt@scramble>
2010-12-23 09:05:40 +0100

    Added to repo.

---
+++ src/dcs/eesim/fwd.hpp
@@ -0,0 +1,57 @@
+#ifndef DCS_EESIM_FWD_HPP
+#define DCS_EESIM_FWD_HPP
+
+
+namespace dcs { namespace eesim {
+
+//template <typename TraitsT>
+//class application_controller;
+
+
+template <typename TraitsT>
+class application_tier;
+
+
+template <typename TraitsT>
+class data_center;
+
+
+template <typename TraitsT>
+class data_center_manager;
+
+
+//template <typename TraitsT>
+//class migration_controller;
+
+
+template <typename TraitsT>
+class multi_tier_application;
+
+
+//template <typename TraitsT>
+//class physical_machine_controller;
+
+
+template <typename TraitsT>
+class physical_machine;
+
+
+template <typename TraitsT>
+class physical_resource;
+
+
+template <typename TraitsT>
+class registry;
+
+
+template <typename TraitsT>
+class virtual_machine;
+
+
+template <typename TraitsT>
+class virtual_machine_monitor;
+
+}} // Namespace dcs::eesim
+
+
+#endif // DCS_EESIM_FWD_HPP
