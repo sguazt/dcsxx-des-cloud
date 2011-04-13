40aeef8929033339fc952875645bd6f48eaef97b
Sguazt <sguazt@scramble>
2011-04-13 15:32:35 +0200

    (maintenance)

--- src/dcs/eesim/proportional_physical_machine_controller.hpp
+++ src/dcs/eesim/proportional_physical_machine_controller.hpp
@@ -132,7 +132,8 @@ class proportional_physical_machine_controller: public base_physical_machine_con
 
 						ptr_vm->resource_share(category, share);
 
-						DCS_DEBUG_TRACE("Assigned new share: VM: " << ptr_vm->name() << " (" << ptr_vm->id() << ") - Category: " << category << " - Threshold: " << threshold << " - Share Sum: " << share_sum << " ==> Wanted: " << share_it->second << " - Got: " << share);//XXX
+						DCS_DEBUG_TRACE("APP: " << ptr_vm->guest_system().application().id() << ", MACH: " << this->machine().id() << " - Assigned new share: VM: " << ptr_vm->name() << " (" << ptr_vm->id() << ") - Category: " << category << " - Threshold: " << threshold << " - Share Sum: " << share_sum << " ==> Wanted: " << share_it->second << " - Got: " << share);//XXX
+::std::cerr << "APP: " << ptr_vm->guest_system().application().id() << ", MACH: " << this->machine().id() << " - Assigned new share: VM: " << ptr_vm->name() << " (" << ptr_vm->id() << ") - Category: " << category << " - Threshold: " << threshold << " - Share Sum: " << share_sum << " ==> Wanted: " << share_it->second << " - Got: " << share << ::std::endl;//XXX
 					}
 				}
 			}
