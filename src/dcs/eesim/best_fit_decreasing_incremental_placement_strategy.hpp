9b91e136608220b5eeb93b11bdc7a2bfda2e3352
Sguazt <sguazt@scramble>
2011-11-05 09:04:20 +0100

    (new|minor) If no enough space is found for some VM, try to allocate such VMs on the machine with the largest free space available.

--- src/dcs/eesim/best_fit_decreasing_incremental_placement_strategy.hpp
+++ src/dcs/eesim/best_fit_decreasing_incremental_placement_strategy.hpp
@@ -32,14 +32,16 @@ class best_fit_decreasing_incremental_placement_strategy: public base_incrementa
 		typedef typename data_center_type::application_type application_type;
 		typedef typename data_center_type::physical_machine_type pm_type;
 		typedef typename data_center_type::physical_machine_pointer pm_pointer;
-		typedef typename pm_type::identifier_type pm_identifier_type;
 		typedef typename data_center_type::virtual_machine_type vm_type;
 		typedef typename data_center_type::virtual_machine_pointer vm_pointer;
+		typedef typename data_center_type::virtual_machines_placement_type vms_placement_type;
+		typedef typename pm_type::identifier_type pm_identifier_type;
 		typedef ::std::vector<pm_pointer> pm_container;
 		typedef typename pm_container::const_iterator pm_iterator;
 		typedef typename vm_container::const_iterator vm_iterator;
-		typedef ::std::pair<physical_resource_category,real_type> share_type;
-		typedef ::std::vector<share_type> share_container;
+		typedef typename application_type::application_tier_type application_tier_type;
+		typedef typename application_tier_type::resource_share_type share_type;
+		typedef typename application_tier_type::resource_share_container share_container;
 		typedef typename share_container::const_iterator share_iterator;
 		typedef ::std::map<physical_resource_category,real_type> resource_share_map;
 		typedef ::std::map<physical_resource_category,real_type> resource_utilization_map;
@@ -91,10 +93,11 @@ class best_fit_decreasing_incremental_placement_strategy: public base_incrementa
 					detail::ptr_virtual_machine_greater_comparator<vm_type>());
 
 DCS_DEBUG_TRACE("BEGIN Incremental Placement");//XXX
+::std::cerr << "[best_fit_decreasing_incremental_placement] BEGIN Incremental Placement" << ::std::endl;//XXX
 DCS_DEBUG_TRACE("#Machines: " << sorted_pms.size());//XXX
 DCS_DEBUG_TRACE("#VMs: " << sorted_vms.size());//XXX
 
-		virtual_machines_placement<traits_type> deployment(dc.current_virtual_machines_placement());
+		vms_placement_type deployment(dc.current_virtual_machines_placement());
 
 		vm_iterator vm_end_it(sorted_vms.end());
 		for (vm_iterator vm_it = sorted_vms.begin(); vm_it != vm_end_it; ++vm_it)
@@ -193,9 +196,92 @@ DCS_DEBUG_TRACE("#VMs: " << sorted_vms.size());//XXX
 											  utils.end(),
 											  dc);
 DCS_DEBUG_TRACE("Placed: VM(" << ptr_vm->id() << ") -> PM(" << ptr_pm->id() << ") ==> OK? " <<  std::boolalpha << placed);///XXX
+::std::cerr << "[best_fit_decreasing_incremental_placement] Placed: VM(" << ptr_vm->id() << ") -> PM(" << ptr_pm->id() << ") with SHARE: " << shares.at(cpu_resource_category) << " ==> OK? " <<  std::boolalpha << placed << ::std::endl;///XXX
+			}
+
+			if (!placed)
+			{
+				// Choose the PM with the largest free space
+				typedef typename vms_placement_type::const_iterator vmp_iterator;
+				typedef typename vms_placement_type::share_const_iterator vmp_share_iterator;
+				typedef ::std::map<pm_identifier_type,resource_share_map> pm_share_map;
+				typedef typename pm_share_map::const_iterator pm_share_iterator;
+				typedef typename resource_share_map::const_iterator resource_share_iterator;
+
+				pm_share_map pm_shares;
+
+				// Compute the free share for each PM
+				vmp_iterator vmp_end_it(deployment.end());
+				for (vmp_iterator vmp_it = deployment.begin(); vmp_it != vmp_end_it; ++vmp_it)
+				{
+					pm_identifier_type pm_id(deployment.pm_id(vmp_it));
+
+					vmp_share_iterator share_end_it(deployment.shares_end(vmp_it));
+					for (vmp_share_iterator share_it = deployment.shares_begin(vmp_it); share_it != share_end_it; ++share_it)
+					{
+						physical_resource_category category(share_it->first);
+
+						if (pm_shares.count(pm_id) && pm_shares.at(pm_id).count(category))
+						{
+							pm_shares[pm_id][category] -= share_it->second;
+						}
+						else
+						{
+							pm_shares[pm_id][category] = 1-share_it->second;
+						}
+					}
+				}
+
+				// Find the PM with the max free share
+				pm_identifier_type max_pm_id;
+				resource_share_map max_pm_shares;
+				pm_share_iterator pm_share_end_it(pm_shares.end());
+				for (pm_share_iterator pm_share_it = pm_shares.begin(); pm_share_it != pm_share_end_it; ++pm_share_it)
+				{
+					pm_identifier_type pm_id(pm_share_it->first);
+
+					resource_share_iterator share_end_it(pm_share_it->second.end());
+					for (resource_share_iterator share_it = pm_share_it->second.begin(); share_it != share_end_it; ++share_it)
+					{
+						physical_resource_category category(share_it->first);
+						real_type free_share(share_it->second);
+
+						//TODO: CPU resource category not yet handle in app simulation model
+						DCS_ASSERT(
+								category == cpu_resource_category,
+								DCS_EXCEPTION_THROW(
+									::std::runtime_error,
+									"Resource categories other than CPU are not yet implemented."
+								)
+							);
+
+						if (free_share > 0 && (!max_pm_shares.count(category) || max_pm_shares.at(category) < free_share))
+						{
+							max_pm_shares[category] = free_share;
+							max_pm_id = pm_id;
+						}
+					}
+				}
+
+				if (max_pm_shares.size() > 0)
+				{
+					pm_pointer ptr_pm(dc.physical_machine_ptr(max_pm_id));
+
+					// paranoid-check: null
+					DCS_DEBUG_ASSERT( ptr_pm );
+
+					// Try to place current VM on current PM
+					placed = deployment.try_place(*ptr_vm,
+												  *ptr_pm,
+												  max_pm_shares.begin(),
+												  max_pm_shares.end());
+	DCS_DEBUG_TRACE("Placed: VM(" << ptr_vm->id() << ") -> PM(" << ptr_pm->id() << ") ==> OK? " <<  std::boolalpha << placed);///XXX
+	::std::cerr << "[best_fit_decreasing_incremental_placement] Placed: VM(" << ptr_vm->id() << ") -> PM(" << ptr_pm->id() << ") with SHARE: " << max_pm_shares.at(cpu_resource_category) << " ==> OK? " <<  std::boolalpha << placed << ::std::endl;///XXX
+				}
 			}
 		}
 
+::std::cerr << "[best_fit_decreasing_incremental_placement] END Incremental Placement" << ::std::endl;//XXX
 DCS_DEBUG_TRACE("END Incremental Placement ==> " << deployment);///XXX
 		return deployment;
 	}
