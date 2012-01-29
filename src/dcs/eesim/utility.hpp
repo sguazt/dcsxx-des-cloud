c040cbab0584b9c67613c6d0774ca02dfe698864
Sguazt <sguazt@scramble>
2012-01-29 14:51:27 +0100

    (bug-fix|major) Remove share from the scaling of utilization.

--- src/dcs/eesim/utility.hpp
+++ src/dcs/eesim/utility.hpp
@@ -158,7 +158,33 @@ RealT scale_resource_utilization(RealT source_capacity,
 								 RealT source_utilization,
 								 RealT target_threshold = ::std::numeric_limits<RealT>::infinity())
 {
-	RealT target_utilization(source_utilization/resource_scaling_factor(source_capacity, source_share, target_capacity, target_share));
+	/// Utilization is insensible to the share.
+	/// As a matter of fact, utilization is computed as busy-time/uptime:
+	/// $U=\frac{B}{T}$
+	///
+	/// Suppose that a VM induces on a physical machine a utilization of $U=0.5$, when it owns the 100% of the physical resource capacity.
+	/// If this VM gets a resource share of $s=0.5$, then the busy-time $B^{\prime}$, approximately, double since the VM will take the double of the time to complete the same operations.
+	/// So, one can think that also the utilization double too, that is $U^{\prime}=1$.
+	///
+	/// However, that formula for computing the utilization assumes that the CPU is always used at 100%.
+	/// Instead, it would be right to estimate the utilization as:
+	/// 	$U=s\frac{B}{T}$
+	/// so that the utilization induced by the VM is relative to the fraction of obtained CPU capacity.
+	/// This is implies that the VMM is working in a non-conservative approach, so that if the physical machine has free capacity -- in excess to the one allocated to the VM -- this capacity is not assigned to the VM).
+	///
+	/// Thus, in the preceeding example we can state that
+	/// $U^{\prime}=0.5\frac{B^{\prime}}{T}=0.5\frac{2B}{T}=\frac{B}{T}=U$
+	/// that is, utilization is insensible to the share.
+	/// Obviously, utilization will remain sensible to the CPU capacity.
+	/// Specifically, if a VM is moved to a machine with capacity $C^{\prime}$ different from the initial one $C$, the scaling of utilization must be take care of this difference, that is:
+	///     $U^{\prime}=U\frac{C}{C^{\prime}}$
+	/// For instance, if the target machine has a capacity $C^{\prime}$ which is double of the initial one $C$, the new utilization is the half of the original one.
+
+	DCS_MACRO_SUPPRESS_UNUSED_VARIABLE_WARNING(source_share);
+	DCS_MACRO_SUPPRESS_UNUSED_VARIABLE_WARNING(target_share);
+
+	//RealT target_utilization(source_utilization/resource_scaling_factor(source_capacity, source_share, target_capacity, target_share));
+	RealT target_utilization(source_utilization/resource_scaling_factor(source_capacity, target_capacity));
 
 	return ::std::isfinite(target_threshold)
 		   ? ::std::min(target_utilization, target_threshold)
